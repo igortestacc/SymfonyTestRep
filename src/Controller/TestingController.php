13a9d1e8c1657cf583a027dbac386e90492476f3
Igor Maiboroda <igorcheckingacc@gmail.com>
2024-07-30 12:31:31 +0300

    using twig

--- src/Controller/TestingController.php
+++ src/Controller/TestingController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 
-class TestingController
+class TestingController extends AbstractController
 {
     #[Route('/')]
     public function getNumber(): Response
@@ -19,15 +20,32 @@ class TestingController
         };
 
         return new Response(
-            '<html><body><h2>Lucky status: ' . $paymentStatusName . '</h2></body></html>'
+            '<h2>Lucky status: ' . $paymentStatusName . '</h2>'
         );
     }
 
     #[Route('/print/{playerUUID}')]
     public function printName(string $playerUUID = null) : Response 
     {
-        $display = '<html><body><h2>Player UUID: ' . $playerUUID . '</h2></body></html>';
+        $display = '<h2>Player UUID: ' . $playerUUID . '</h2>';
 
-        return new Response($display);
+        $randomArr = [];
+        for ($i = 0; $i < 12; $i++) {
+            $randomArr[] = random_int(-100, 100);
+        }
+
+        $fullNames = [
+          ['name' => 'Igor', 'lastName' => 'Maiboroda'],
+          ['name' => 'Vlad', 'lastName' => 'Somov'],
+          ['name' => 'Ihor', 'lastName' => 'Pryschepa'],
+        ];
+
+        //return new Response($display);
+        return $this->render('testing/printName.html.twig', [
+            'title' => 'Page Name',
+            'username' => $playerUUID,
+            'numbers' => $randomArr,
+            'fullNames' => $fullNames,
+        ]);
     }
 }
