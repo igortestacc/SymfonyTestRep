5826dbe246a21f608a093a1b19599da604507aec
Igor Maiboroda <igorcheckingacc@gmail.com>
2024-08-01 19:00:14 +0300

    requests and query builder

--- src/Controller/DataController.php
+++ src/Controller/DataController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\UserNew;
+use App\Repository\UserNewRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -28,5 +31,53 @@ class DataController extends AbstractController
 
         //return new JsonResponse($user);
         return $this->json($user);
+    }
+
+    #[Route('/api/new', name: 'api_new')]
+    public function new(EntityManagerInterface $entityManager): Response
+    {
+        $userDB = new UserNew();
+        $userDB->setUsername('sombo1');
+        $userDB->setAge(18);
+
+        $entityManager->persist($userDB);
+        $entityManager->flush();
+
+        return new Response(sprintf('New user with id: %d added!', $userDB->getId()));
+    }
+
+    #[Route('/api/getAll/{username}', name: 'api_getAll')]
+    public function getAll(UserNewRepository $newRepository, $username)
+    {
+        //$users_new = $newRepository->findAll();
+        //$users_new = $newRepository->findBy(['username' => $username], ['dateBirth' => 'DESC']);
+        $users_new = $newRepository->findAllByUsername($username);
+        dd($users_new);
+    }
+
+    #[Route('/api/show/{id}', name: 'api_show')]
+    public function show(UserNewRepository $newRepository, $id)
+    {
+        $user_new = $newRepository->find($id);
+
+        if (!$user_new) {
+            throw $this->createNotFoundException('User new not found');
+        }
+
+        dd($user_new);
+    }
+
+    #[Route('/api/show_other/{id}/age', name: 'api_show_other', methods: ['POST'])]
+    public function show_other(UserNew $userNew, Request $request)
+    {
+        $direction = $request->get('direction');
+
+        if ($direction == 'up') {
+            $userNew->setAge($userNew->getAge() + 1);
+        } elseif ($direction == 'down') {
+            $userNew->setAge($userNew->getAge() - 1);
+        }
+
+        dd($userNew);
     }
 }
---
+++ src/Repository/UserNewRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\UserNew;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<UserNew>
+ */
+class UserNewRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, UserNew::class);
+    }
+
+    //    /**
+    //     * @return UserNew[] Returns an array of UserNew objects
+    //     */
+
+    public function findAllByUsername(string $name = null): array
+    {
+        $queryBuilder = $this->createQueryBuilder('usernew')
+            ->orderBy('usernew.id', 'DESC');
+
+        if ($name != null) {
+            $queryBuilder->andWhere('usernew.username LIKE :name')
+                ->setParameter('name', $name);
+        }
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('u')
+    //            ->andWhere('u.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('u.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?UserNew
+    //    {
+    //        return $this->createQueryBuilder('u')
+    //            ->andWhere('u.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
